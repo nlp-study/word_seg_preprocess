ce8b8be28555cd99d62c0744dbc0648a623684f4
Xiulong <zhang_seu@163.com>
2017-01-20 15:38:30 +0800

    add a new package

--- src/corpus_statics/word_statics.py
+++ src/corpus_statics/word_statics.py
@@ -4,5 +4,6 @@ Created on 2017年1月18日
 @author: Zhang Xiulong
 '''
 
+
 if __name__ == '__main__':
     pass
---
+++ src/word_seg_result_compare/word_seg_result_text_compare.py
@@ -0,0 +1,10 @@
+'''
+Created on Jan 20, 2017
+
+@author: zhang
+'''
+def compare_two_files(input_path_1,input_path_2):
+    pass
+    
+if __name__ == '__main__':
+    pass
